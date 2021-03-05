e3d19266b6cdce540e9d20bd18d5e86858b87dcb
theblackpowder <dylan.schaefer@ndus.edu>
2021-03-05 10:51:45 -0600

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   name = "hello world";
   isSpecial: boolean = true;
   messageWelcome: string = "";
+  username: string = "";
   redColor = "Red";
   imageURL =
     "https://i.gaw.to/content/photos/43/39/433990-cette-subaru-impreza-22b-sti-est-a-vendre-pour-un-demi-million.jpg?460x287";
